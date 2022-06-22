008d2febce3a40f0c3e21f3c4043a60507aa7578
aamrlyman <aamrlyman@gmail.com>
2022-06-22 14:14:13 -0700

    Basic modules created

---
+++ robot.py
@@ -0,0 +1,11 @@
+class Robot: 
+
+    def __init__(self):
+
+        self.name = 'Robot'
+        self.health = 100
+        self.active_weapon = {}
+
+    def attack(self, dinosaur):
+         
+    

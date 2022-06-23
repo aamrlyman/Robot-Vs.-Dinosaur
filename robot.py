5f11307b58519b3ec641afb70150bf60a7b19f6d
aamrlyman <aamrlyman@gmail.com>
2022-06-22 20:51:27 -0700

    Got weapon selection working!

--- robot.py
+++ robot.py
@@ -8,9 +8,11 @@ class Robot:
 
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon('Plasma Sword')
+        self.weapons = [Weapon('Plasma Sword'), Weapon('Laser Gun'), Weapon('Grenade')]
+        self.active_weapon = self.weapons[1]
 
     def attack(self, Opponent):
+        self.active_weapon = self.weapons[int(input('Which weapon you want to use? 0 = Plasma swoard, 1 = Laser Gun, 2 = Grenade: '))]
         Opponent.health = Opponent.health - self.active_weapon.attack_power
         if Opponent.health < 0:
             Opponent.health = 0
